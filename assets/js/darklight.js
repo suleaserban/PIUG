cccce97e3b8b979410190a6e5d9dbffb2d3e7de0
Sulea Serban <serban.sulea@ibm.com>
2023-11-16 18:33:25 +0200

    commit

---
+++ assets/js/darklight.js
@@ -0,0 +1,9 @@
+document.getElementById("mode-toggle").addEventListener('change', function() {
+  if(this.checked) {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  } else {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+});
